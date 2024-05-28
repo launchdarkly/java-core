d1f0434c69d3244a2de82930daaab45149413870
Todd Anderson <127344469+tanderson-ld@users.noreply.github.com>
2024-05-28 17:24:14 -0500

    ci: updating nexus publish plugin version (#26)
    Updating nexus plugin

--- lib/shared/internal/build.gradle.kts
+++ lib/shared/internal/build.gradle.kts
@@ -1,9 +1,5 @@
 import java.time.Duration
 
-// These values come from gradle.properties
-val ossrhUsername: String by project
-val ossrhPassword: String by project
-
 buildscript {
     repositories {
         mavenCentral()
@@ -86,10 +82,7 @@ nexusStaging {
 nexusPublishing {
     clientTimeout.set(Duration.ofMinutes(2)) // we've seen extremely long delays in creating repositories
     repositories {
-        sonatype {
-            username.set(ossrhUsername)
-            password.set(ossrhPassword)
-        }
+        sonatype()
     }
 }
 
