5d6f9eaa83dc35bd5566cb931cbeacb50fbdf37f
Todd Anderson <tanderson@launchdarkly.com>
2024-10-23 11:02:47 -0500

    reverting Version.java change

--- lib/sdk/server/src/main/java/com/launchdarkly/sdk/server/Version.java
+++ lib/sdk/server/src/main/java/com/launchdarkly/sdk/server/Version.java
@@ -4,5 +4,7 @@ abstract class Version {
   private Version() {}
   
   // This constant is updated automatically by our Gradle script during a release, if the project version has changed
+  // x-release-please-start-version
   static final String SDK_VERSION = "7.5.0";
+  // x-release-please-end
 }
