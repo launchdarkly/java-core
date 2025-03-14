b9a6f4c7b737d081b5b55972d66e388ccddf97ca
Todd Anderson <127344469+tanderson-ld@users.noreply.github.com>
2025-03-14 12:08:23 -0500

    chore: migrating redis store package to java-core (#45)
    Migrates redis-store package to java-core package.

---
+++ lib/java-server-sdk-redis-store/src/test/java/com/launchdarkly/sdk/server/integrations/RedisBigSegmentStoreImplTest.java
@@ -0,0 +1,51 @@
+package com.launchdarkly.sdk.server.integrations;
+
+import com.launchdarkly.sdk.server.subsystems.BigSegmentStore;
+import com.launchdarkly.sdk.server.subsystems.BigSegmentStoreTypes;
+import com.launchdarkly.sdk.server.subsystems.ComponentConfigurer;
+
+import redis.clients.jedis.Jedis;
+
+@SuppressWarnings("javadoc")
+public class RedisBigSegmentStoreImplTest extends BigSegmentStoreTestBase {
+
+  @Override
+  protected ComponentConfigurer<BigSegmentStore> makeStore(String prefix) {
+    return Redis.bigSegmentStore().prefix(prefix);
+  }
+
+  @Override
+  protected void clearData(String prefix) {
+    prefix = prefix == null || prefix.isEmpty() ? RedisStoreBuilder.DEFAULT_PREFIX : prefix;
+    try (Jedis client = new Jedis("localhost")) {
+      for (String key : client.keys(prefix + ":*")) {
+        client.del(key);
+      }
+    }
+  }
+
+  @Override
+  protected void setMetadata(String prefix, BigSegmentStoreTypes.StoreMetadata storeMetadata) {
+    try (Jedis client = new Jedis("localhost")) {
+      client.set(prefix + ":big_segments_synchronized_on",
+          storeMetadata != null ? Long.toString(storeMetadata.getLastUpToDate()) : "");
+    }
+  }
+
+  @Override
+  protected void setSegments(String prefix,
+                             String userHashKey,
+                             Iterable<String> includedSegmentRefs,
+                             Iterable<String> excludedSegmentRefs) {
+    try (Jedis client = new Jedis("localhost")) {
+      String includeKey = prefix + ":big_segment_include:" + userHashKey;
+      String excludeKey = prefix + ":big_segment_exclude:" + userHashKey;
+      for (String includedSegmentRef : includedSegmentRefs) {
+        client.sadd(includeKey, includedSegmentRef);
+      }
+      for (String excludedSegmentRef : excludedSegmentRefs) {
+        client.sadd(excludeKey, excludedSegmentRef);
+      }
+    }
+  }
+}
