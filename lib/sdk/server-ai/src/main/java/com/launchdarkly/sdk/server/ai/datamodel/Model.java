0a8ad3fa4798651187cce0439712f6c67d85da7f
Louis Chan <lchan@launchdarkly.com>
2025-05-08 15:07:20 -0700

    chore: Convert JSON LDValue into the AiConfig Object

---
+++ lib/sdk/server-ai/src/main/java/com/launchdarkly/sdk/server/ai/datamodel/Model.java
@@ -0,0 +1,39 @@
+package com.launchdarkly.sdk.server.ai.datamodel;
+
+import java.util.HashMap;
+
+import com.launchdarkly.sdk.LDValue;
+
+public class Model {
+    private String name;
+
+    private HashMap<String, LDValue> parameters;
+
+    private HashMap<String, LDValue> custom;
+
+    // Getters and Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public HashMap<String, LDValue> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(HashMap<String, LDValue> parameters) {
+        this.parameters = parameters;
+    }
+
+    public HashMap<String, LDValue> getCustom() {
+        return custom;
+    }
+
+    public void setCustom(HashMap<String, LDValue> custom) {
+        this.custom = custom;
+    }
+}
---
+++ lib/sdk/server-ai/src/main/java/com/launchdarkly/sdk/server/ai/datamodel/Provider.java
@@ -0,0 +1,13 @@
+package com.launchdarkly.sdk.server.ai.datamodel;
+
+public class Provider {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
