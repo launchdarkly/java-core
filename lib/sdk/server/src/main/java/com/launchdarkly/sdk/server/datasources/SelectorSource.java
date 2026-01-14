3428591b7236e0cf69234c6fa17353682e8cd205
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2026-01-14 11:16:40 -0800

    Add comments to SelectorSource.

--- lib/sdk/server/src/main/java/com/launchdarkly/sdk/server/datasources/SelectorSource.java
+++ lib/sdk/server/src/main/java/com/launchdarkly/sdk/server/datasources/SelectorSource.java
@@ -2,6 +2,15 @@ package com.launchdarkly.sdk.server.datasources;
 
 import com.launchdarkly.sdk.internal.fdv2.sources.Selector;
 
+/**
+ * This type is currently experimental and not subject to semantic versioning.
+ * <p>
+ * Source of selectors for FDv2 implementations.
+ */
 public interface SelectorSource {
+    /**
+     * Get the current selector.
+     * @return The current selector.
+     */
     Selector getSelector();
 }
