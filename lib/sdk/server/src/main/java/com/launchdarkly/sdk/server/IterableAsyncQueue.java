ff02c1e1fe27a2b7234b3db229d37055235133ec
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2026-01-15 09:46:43 -0800

    Don't double queue async items

--- lib/sdk/server/src/main/java/com/launchdarkly/sdk/server/IterableAsyncQueue.java
+++ lib/sdk/server/src/main/java/com/launchdarkly/sdk/server/IterableAsyncQueue.java
@@ -14,6 +14,7 @@ class IterableAsyncQueue<T> {
             if(nextFuture != null) {
                 nextFuture.complete(item);
                 nextFuture = null;
+                return;
             }
             queue.addLast(item);
         }
